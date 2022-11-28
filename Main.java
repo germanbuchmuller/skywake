c207ff9f2ac77ea4831f52a959cf9577e8eead9c
Germán Buchmüller <german.buchmuller@ing.austral.edu.ar>
2022-11-28 20:09:22 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.application.Platform;
 
+public class HelloFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Button btn = new Button("Exit");
+        btn.setOnAction(actionEvent -> Platform.exit());
+        Scene scene = new Scene(new VBox(l, btn), 1920, 1080);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
